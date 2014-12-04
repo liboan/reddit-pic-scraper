9a5d4b67c74f85fea46e99c188f45f628871b8e3
Andrew <liboan1997@gmail.com>
2014-12-03 23:20:45 -0500

    successfully decoded JSON string into python dict

--- download-test.py
+++ download-test.py
@@ -1,6 +1,13 @@
 import urllib3
+import json
 
 http = urllib3.PoolManager()
-r = http.request('GET',"http://www.reddit.com/r/WorldofTanks/.json")
+r = http.request('GET',"http://www.reddit.com/r/civ/.json") #gets JSON from url
 print(r.status)
-print(r.data)
+
+print(type(r.data))
+data = r.data.decode("ascii")
+
+data = json.loads(data)
+print(type(data))
+print(data["data"]["children"][0]["kind"]) #going to have to do some crazy dict stuff

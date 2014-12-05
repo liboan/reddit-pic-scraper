e0ee340b5f9851553f0c81b890aeb94754d94ff2
Andrew <liboan1997@gmail.com>
2014-12-04 22:15:42 -0500

    can print various attributes of JSON objects

--- download-test.py
+++ download-test.py
@@ -2,7 +2,7 @@ import urllib3
 import json
 
 http = urllib3.PoolManager()
-r = http.request('GET',"http://www.reddit.com/r/civ/.json") #gets JSON from url
+r = http.request('GET',"http://www.reddit.com/r/EarthPorn/.json") #gets JSON from url
 print(r.status)
 
 print(type(r.data))
@@ -10,4 +10,7 @@ data = r.data.decode("ascii")
 
 data = json.loads(data)
 print(type(data))
-print(data["data"]["children"][0]["kind"]) #going to have to do some crazy dict stuff
+#print(data["data"]["children"][0]["kind"]) #going to have to do some crazy dict stuff
+
+for item in data["data"]["children"]:
+	print("%d \t %s \t %s" % (item["data"]["score"], item["data"]["domain"],  item["data"]["url"]))

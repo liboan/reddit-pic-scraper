743ebc8f93a187a333bd19a84ae62b68f6df4850
Andrew <liboan1997@gmail.com>
2014-12-06 22:19:31 -0500

    created methods for fetching image, checking if url linked to an image

--- image-test.py
+++ image-test.py
@@ -1,19 +1,41 @@
 import urllib3
 import json
+from datetime import datetime
+import re
 
 http = urllib3.PoolManager()
-imgURL = "http://i.imgur.com/XDvF40M.jpg"
-r = http.request("GET", imgURL)
-image = r.data
+imgURL = "http://i.imgur.com/1qzb8tn.jpg?1"
+# r = http.request("GET", imgURL)
+# image = r.data
 
-file = open("test.jpg",'wb')
-file.write(image)
-file.close()
+def checkURL(url):
+	isPicture = re.compile(r".(jpg|png|gif)")
+	if isPicture.search(url):
+		return True
+	else:
+		return False
 
-logObj = {"url": imgURL, "name": "test"}
+def fetchImage(url, name):
+	request = http.request("GET", url)
+	image = request.data
+	file = open(name+".jpg", 'wb')
+	file.write(image)
+	file.close()
+	
 
-print(json.dumps(logObj))
+# file = open("test.jpg",'wb')
+# file.write(image)
+# file.close()
 
-jsonLog = open("log.json","w")
-json.dump(logObj,jsonLog)
-jsonLog.close()
+if checkURL(imgURL):
+	fetchImage(imgURL,"picture")
+	logObj = {"url": imgURL, "name": "test", "timeFetched": str(datetime.now())}
+	print(datetime.now().replace(microsecond=0))
+	print(json.dumps(logObj))
+	jsonLog = open("log.json","w")
+	json.dump(logObj,jsonLog)
+	jsonLog.close()
+
+else:
+	print("NOT AN IMAGE")
+
